a85301b59757306f1b9dbedc26980845f8a23dc5
Claude Pageau <pageauc@gmail.com>
2015-01-11 19:53:15 -0600

    revamped makedailymovie.sh
    Changed script to use variables and use original motion folder to build
    image file list dailymovie.txt for mencoder to process.  Also reversed
    order to get movie to play from oldest to newest order.

--- source/makedailymovie.sh
+++ source/makedailymovie.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# makedailymovie.sh - written by Claude Pageau based on script by zengargoyle
-# This script will move files from /home/pi/pi-timolo/timelapse to /home/pi/pi-timolo/dailymoves
-# It will then use mencoder to create a move and move it to /home/pi/pi-timolo/mnt
-# make sure you create the required folders and create the fstab entry to mount
+# makedailymovie.sh - written by Claude Pageau.
+# Skip most recent file logic based on script by zengargoyle
+# This script will create a daily movie from jpg files in motion folder
+# It will then use mencoder to create avi movie and move it to a mount point
+# make sure you create the required mnt folders and create the fstab entry to mount
 # the remote share onto /home/pi/pi-timolo/mnt.  This folder MUST be empty before the share will mount.
-# verify the share point is mounted before running this script.
-# Note this script does not delete files in /home/pi/pi-timolo/dailymovies folder.  
-# This line can be added to the bottom of the script otherwise if something goes wrong
-# the files will be deleted event if the process fails since there is no error checking
-# in the script.  I leave it to you to add this if required.
+# Verify the share point is mounted before running this script.
+# Note the previous daily movie will be deleted when this script is run so you have 24 hours to
+# fix any copy problem to external share mount point folder.
 #
 # Mounting a network share to /home/pi/pi-timolo/mnt folder
 # it still might be updating.  This avoids a rpi-timelapse.py crash
@@ -20,53 +19,80 @@
 #
 # sudo nano /etc/fstab
 #
-# Add a similar line below to the fstab.  
+# Add a similar line below to the fstab.
 # This example mounts an external Hard Drive share
-# on a RPI running openelec xbmc.  
+# on a RPI running openelec xbmc.
 # Change the IP address, share name and paths appropriately (exclude the #)
 #
 # //192.168.1.154/sharename/Media/Images /home/pi/pi-timolo/mnt cifs username=root,password=openelec,uid=pi,gid=pi, 0 0
 #
-# Add a crontab entry to the root crontab per the below.  
+# Add a crontab entry to the root crontab per the below.
 # make sure makedailymovie.sh is executable eg sudo chmod +x makedailymovie.sh
 #
 # sudo crontab -e
 #
-# Add similar crontab entry as line below (excluding the #). 
+# Add similar crontab entry as line below (excluding the #).
 # This would execute makedailymovie.sh at 10pm every day
 #
 # 01 20 * * * /home/pi/pi-timolo/makedailymovie.sh
 
-sudo rm /home/pi/pi-timolo/dailymovie/*jpg
-files_to_check=/home/pi/pi-timolo/motion/*jpg
-command_to_run='cp -u'
-the_destination=/home/pi/pi-timolo/dailymovie
+# sudo rm /home/pi/pi-timolo/dailymovie/*jpg
 
-# looking for files matching $files_to_check
+# get current working folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+folder_source=$DIR/motion
+folder_destination=$DIR/mnt
+moviename=$DIR/dailymovie_$(date '+%Y%m%d').avi
+movielist=$DIR/dailymovie.txt
+
+clear
+echo $0 " Working ..."
+echo "=========================================================================="
+echo "Movie Name  =" $moviename
+echo "Source      =" $folder_source
+echo "Destination =" $folder_destination
+echo "Image List  =" $movielist
+echo "=========================================================================="
+
+cd $DIR
+
+if [ -f $movielist ]; then
+   echo "Delete "$movielist
+   rm $movielist
+fi
+
+if [ -f $DIR/*avi ]; then
+   rm $DIR/*avi 
+fi
+
+echo "Create jpg Image files List "$movielist
+echo "From Folder "$folder_source
+
 # ls -t sorts files by last modification time, most recent to oldest
-
-ls -t $files_to_check |
+ls -t $folder_source/*.jpg |
 (
-
-  # the first line will be the most recent file
-
+  # the first line will be the most recent file so ignore it
+  # since it might still be in progress
   read the_most_recent_file
-  # do nothing with the most recent file: $the_most_recent_file
-
-
-  # the rest of the lines will not be the most recent file
-
+     # Skip this file in listing
   # do something with the rest of the files
   while read not_the_most_recent_file
   do
-    $command_to_run $not_the_most_recent_file $the_destination
+     echo $not_the_most_recent_file >> $DIR/dailymovie.tmp
   done
 )
-cd /home/pi/pi-timolo
 
-moviename=dailymovie_$(date '+%Y%m%d').avi
+# reverse the list order so the video runs from oldest to most recent
+tac $DIR/dailymovie.tmp > $movielist
 
-ls -tr $the_destination/*jpg > dailymovie.txt
-/usr/bin/mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1280:720 -o $moviename -mf type=jpeg:fps=5  mf://@dailymovie.txt
+if [ -f $DIR/dailymovie.tmp ]; then
+   rm $DIR/dailymovie.tmp
+fi
 
-mv $moviename /home/pi/pi-timolo/mnt/
+echo "Making Daily Movie ... "$moviename
+echo "=========================================================================="
+/usr/bin/mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1280:720 -o $moviename -mf type=jpeg:fps=5  mf://@$movielist
+echo "=========================================================================="
+echo "Copy "$moviename" to "$folder_destination
+cp $moviename $folder_destination
+echo "Processing Complete ..."
