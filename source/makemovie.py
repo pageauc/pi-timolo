76fddae1d7507feff281220f8cf25fc85d2b5fa7
Claude Pageau <pageauc@gmail.com>
2015-07-20 16:39:47 -0400

    updated makemovie.py to be python3 compliant
    did a
    2to3 -w makemovie.py to update print statements so it will be python 3
    compliant.

--- source/makemovie.py
+++ source/makemovie.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # written by Claude Pageau for use by pitimo.py image files.
 
-print "initializing ...."
-import StringIO
+print("initializing ....")
+import io
 import subprocess
 import os
 import time
@@ -22,23 +22,23 @@ aspectRatio = "16/9"
 # Video fps (frames per second) vulue usually  between 2 to 30.  I recommend 5 fps to start
 framesPerSec = 5
 
-print "makemovie.py"
-print "============"
-print "Creating makemovie.txt file of *jpg files in google_drive folder." 
+print("makemovie.py")
+print("============")
+print("Creating makemovie.txt file of *jpg files in google_drive folder.") 
 ls_params = " -t -r ./%s/*jpg > makemovie.txt" % imagePath
 exit_status = subprocess.call("ls %s " % ls_params, shell=True)
 
-print "Creating movie file %s using makemovie.txt" % ( movieName )
-print "Settings = Image W=%s H=%s aspect=%s fps=%s filename=%s" % ( imageWidth, imageHeight, aspectRatio, framesPerSec, movieName )
+print("Creating movie file %s using makemovie.txt" % ( movieName ))
+print("Settings = Image W=%s H=%s aspect=%s fps=%s filename=%s" % ( imageWidth, imageHeight, aspectRatio, framesPerSec, movieName ))
 
 mencoder_params = "-nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=%s:vbitrate=8000000 -vf scale=%s:%s -o %s -mf type=jpeg:fps=%s  mf://@makemovie.txt" % ( aspectRatio, imageWidth, imageHeight, movieName, framesPerSec )
-print "memcoder_params = %s" % ( mencoder_params )
-print "Creating Movie. This will take a while ......."
-print "----------------------------------------------"
+print("memcoder_params = %s" % ( mencoder_params ))
+print("Creating Movie. This will take a while .......")
+print("----------------------------------------------")
 
 
 exit_status = subprocess.call("mencoder %s" % mencoder_params, shell=True)
-print "makemovie.py"
-print "============"
-print "Finished timelapse movie filename=%s" % ( movieName )
+print("makemovie.py")
+print("============")
+print("Finished timelapse movie filename=%s" % ( movieName ))
 
