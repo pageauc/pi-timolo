17ceedab9c2462055dc1e76938de1b70f11febaf
Claude Pageau <pageauc@gmail.com>
2016-09-26 12:33:53 -0400

    version 1.5 Updated to be more generic
    Made this script so it can be used for other purposes besides only
    syncing pi-timolo files

--- source/sync.sh
+++ source/sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# version 1.4
-# sync.sh written by Claude Pageau.  Requires /usr/local/bin/gdrive executable compiled from github source for arm
+echo "$0 version 1.5 by Claude Pageau"
+# --------------------------------------------------------------------
+# Requires /usr/local/bin/gdrive executable compiled from github source for arm
 # Note gdrive is included with pi-timolo on github at https://github.com/pageauc/pi-timolo
 # for gdrive details see https://github.com/odeke-em/drive
 # To manually install gdrive binary perform the following.
@@ -21,53 +22,61 @@
 # Add appropriate line to crontab using command sudo crontab -e
 # example crontab entry below without # comment char
 # */5 * * * * /home/pi/sync.sh >/dev/nul
+# --------------------------------------------------------------------
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # folder location of this script
 
-# pi-timolo subfolder where motion files are located
-SYNC_DIR=motion
+# -------------------  User Variables ------------------
+SYNC_DIR=motion          # folder location of files to sync
+FILES_TO_SYNC='*jpg'     # Set the type of files to sync * for all
+CHECK_FOR_SYNC_FILE=true # true if sync file required otherwise set to false
+SYNC_FILE_PATH=$DIR/pi-timolo.sync  # name of pi-timolo sync lock filename
+FORCE_REBOOT=false       # true to reboot if pi-timolo not running otherwise set to false
+# ------------------------------------------------------
 
-echo "$0 version 1.4" 
-# Get current folder where this script is located
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# name of pi-timolo sync file
-SYNC_FILE=pi-timolo.sync
+function do_gdrive_sync()
+{
+  cd $DIR
+  date
+  echo "Start synchronization ....."
+  START=$(date +%s)
+  echo "sudo /usr/local/bin/gdrive push -no-prompt -ignore-conflict $SYNC_DIR/*jpg"
+  sudo /usr/local/bin/gdrive push -no-prompt -ignore-conflict $SYNC_DIR/$FILES_TO_SYNC
+  # Check if gdrive sync was successfully
+  if [ $? -ne 0 ] ;  then
+    echo "ERROR - gdrive Processing failed."
+    echo "  Possible Cause - No internet connection or some other reason."
+  else
+    # If successful then display processing time and remove sync file
+    date
+    echo "SUCCESS - $0 Processing completed successfully"
+    END=$(date +%s)
+    DIFF=$((END - START))
+    echo "Processing took $DIFF seconds"
+    echo "  Deleting lock file $SYNC_FILE"
+    rm -f $SYNC_FILE
+  fi
+}
+
 # check if gdrive is already running to avoid multiple instances
-if [ -z "$(pgrep gdrive)" ]
-  then
-    # Check for sync file indicating pi-timolo.py has new saved image files
-    if [ -e $DIR/$SYNC_FILE ]
-    then
+if [ -z "$(pgrep gdrive)" ] ; then
+  if [ $CHECK_FOR_SYNC_FILE ; then
+    if [ -e $SYNC_FILE_PATH ] ; then
       # Run gdrive for files in folder specified by variable $SYNC_DIR
       echo "---------------------- PROCESSING ---------------------------------"
       echo "Found sync lock file $DIR/$SYNC_FILE"
       echo "Starting gdrive Push From $DIR/$SYNC_DIR"
       echo "                      To  google drive subfolder $SYNC_DIR"
       echo "-------------------------------------------------------------------"
-      cd $DIR
-      date
-      echo "Start synchronization ....."
-      START=$(date +%s)
-      echo "sudo /usr/local/bin/gdrive push -no-prompt -ignore-conflict $SYNC_DIR/*jpg"
-      sudo /usr/local/bin/gdrive push -no-prompt -ignore-conflict $SYNC_DIR/*jpg
-      # Check if gdrive sync was successfully
-      if [ $? -ne 0 ]
-      then
-        echo "ERROR - gdrive Processing failed."
-        echo "  Possible Cause - No internet connection or some other reason."
-      else
-        # If successful then display processing time and remove sync file
-        date
-        echo "SUCCESS - $0 Processing completed successfully"
-        END=$(date +%s)
-        DIFF=$((END - START))
-        echo "Processing took $DIFF seconds"
-        echo "  Deleting lock file $DIR/$SYNC_FILE"
-        rm -f $DIR/$SYNC_FILE
-      fi
+      do_gdrive_sync
     else
-      echo "Sync lock File Not Found $DIR/$SYNC_FILE"
-      echo "  No files to process in $DIR/$SYNC_DIR"
+      echo "Sync lock File Not Found at $SYNC_FILE"
+      echo "  No files to process in $SYNC_DIR"
+      echo ""
     fi
-    cd $DIR
+  else
+    do_gdrive_sync
+  fi
+  cd $DIR
 else
   # gdrive is already running so check how long and kill if past time limit
   if [ -z "$(sudo ps axh -O etimes | grep gdrive | grep -v grep | sed 's/^ *//'| awk '{if ($2 >= 4000) print $1}')" ]
@@ -83,14 +92,14 @@ else
   fi
 fi
 
-# Check if pi-timolo is running and if not then reboot
-echo "Check pi-timolo.py Run Status ..."
-if [ -z "$(pgrep pi-timolo)" ]
-  then
+if $FORCE_REBOOT ; then  # check if reboot required
+  echo "Check pi-timolo.py Run Status ..."
+  if [ -z "$(pgrep pi-timolo)" ] ; then
     echo "pi-timolo.py is NOT running so reboot"
     sudo reboot
   else
     echo "  pi-timolo.py is Running"
+  fi
+  echo "Done ..."
 fi
-echo "Done ..."
 exit
