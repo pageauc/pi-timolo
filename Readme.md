fd4850d072fa9c406aa8d2f92dc043885bba4829
Claude Pageau <pageauc@gmail.com>
2016-10-11 21:40:48 -0400

    Added new pi-timolo-install.sh
    Will delete install.sh after testings.

--- Readme.md
+++ Readme.md
@@ -1,13 +1,32 @@
 # pi-timolo
 ### Raspberry (Pi) - (Ti)melapse, (Mo)tion, (Lo)wlight
 
-For easy pi-timolo install onto raspbian RPI. 
+### Quick Install
+For Easy pi-timolo-install.sh onto raspbian RPI. 
 
-    curl -L https://raw.github.com/pageauc/pi-timolo/master/source/install.sh | bash
+    curl -L https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo-install.sh | bash
 
+#### or Manual Install   
+From logged in RPI SSH session or console terminal perform the following.
+
+    wget https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo-install.sh
+    chmod +x pi-timolo-install.sh
+    ./pi-timolo-install.sh
+    cd pi-timolo
+    ./pi-timolo.py
+
+NOTE - You may have to run sudo ./pi-timolo.py depending on permissions       
 From a computer logged into the RPI via ssh(Putty) session use mouse to highlight command above, right click, copy.  
 Then select ssh(Putty) window, mouse right click, paste.  The command should 
-download and execute the github install.sh script for pi-timolo.
+download and execute the github pi-timolo-install.sh script for pi-timolo program   
+Note - a raspbian apt-get update and upgrade will be performed as part of install 
+so it may take some time if these are not up-to-date
+
+#### For Detailed Instructions See pi-timolo Wiki at https://github.com/pageauc/pi-timolo/wiki   
+
+### Links
+
+YouTube Videos https://www.youtube.com/playlist?list=PLLXJw_uJtQLa11A4qjVpn2D2T0pgfaSG0
 
 ### Description
 pi-timolo is a python picamara module application for a Raspberry PI computer.
@@ -18,8 +37,6 @@ setting, so a real time clock is not required. Customization settings are saved 
 The application is primarily designed for headless operation and include sync.sh that
 can securely synchronize files with a users google drive.  This works well for remote security
 cameras.  
-
-### For Details See [pi-timolo Wiki](https://github.com/pageauc/pi-timolo/wiki) and [YouTube Videos](https://www.youtube.com/playlist?list=PLLXJw_uJtQLa11A4qjVpn2D2T0pgfaSG0)
         
 Good Luck
 Claude Pageau 
---
+++ source/pi-timolo-install.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+# Convenient pi-timolo-install.sh script written by Claude Pageau 1-Jul-2016
+ver="2.3"
+TIMOLO_DIR='pi-timolo'  # Default folder install location
+
+cd ~
+if [ -d "$TIMOLO_DIR" ] ; then
+  STATUS="Upgrade"
+  echo "Upgrade rpi-speed-camera files"
+else  
+  echo "New rpi-speed-camera Install"
+  STATUS="New Install"
+  mkdir -p $TIMOLO_DIR
+  echo "$TIMOLO_DIR Folder Created"
+fi 
+
+cd $TIMOLO_DIR
+INSTALL_PATH=$( pwd )
+
+# Remember where this script was launched from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "-------------------------------------------------------------"
+echo "      pi-timolo Install.sh script ver $ver"
+echo "Install or Upgrade pi-timolo Pi, Timelapse, Motion, Low Light"
+echo "-------------------------------------------------------------"
+echo "1 - Downloading pi-timolo github repo files"
+echo ""
+if [ -e config.py ]; then
+  if [ ! -e config.py.orig ]; then
+     echo "Save config.py to config.py.orig"
+     cp config.py config.py.orig
+  fi
+  echo "Backup config.py to config.py.prev"
+  cp config.py config.py.prev  
+fi
+wget -O config.py -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/config.py
+if [ $? -ne 0 ] ;  then
+  wget -O config.py https://raw.github.com/pageauc/pi-timolo/master/source/config.py
+  wget -O pi-timolo.py https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo.py
+  wget -O pi-timolo-install.sh https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo-install.sh
+  wget -O Readme.md https://raw.github.com/pageauc/pi-timolo/master/Readme.md
+  wget -O install.sh https://raw.github.com/pageauc/pi-timolo/master/source/install.sh
+  wget -O sync.sh https://raw.github.com/pageauc/pi-timolo/master/source/sync.sh
+  wget -O webserver.py https://raw.github.com/pageauc/pi-timolo/master/source/webserver.py
+  wget -O makedailymovie.sh https://raw.github.com/pageauc/pi-timolo/master/source/makedailymovie.sh
+  wget -O makemovie.sh https://raw.github.com/pageauc/pi-timolo/master/source/makemovie.sh
+  wget -O mvleavelast.sh https://raw.github.com/pageauc/pi-timolo/master/source/mvleavelast.sh
+  wget -O myip.sh https://raw.github.com/pageauc/pi-timolo/master/source/myip.sh
+  wget -O gdrive https://raw.github.com/pageauc/pi-timolo/master/source/gdrive
+else
+  wget -O pi-timolo.py -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo.py
+  wget -O pi-timolo.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo.sh
+  wget -O Readme.md -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/Readme.md
+  wget -O pi-timolo-install.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/pi-timolo-install.sh
+  wget -O sync.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/sync.sh
+  wget -O webserver.py -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/webserver.py
+  wget -O makedailymovie.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/makedailymovie.sh
+  wget -O makemovie.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/makemovie.sh
+  wget -O mvleavelast.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/mvleavelast.sh
+  wget -O myip.sh -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/myip.sh
+  wget -O gdrive -q --show-progress https://raw.github.com/pageauc/pi-timolo/master/source/gdrive
+fi  
+echo "Done Download"
+echo "-------------------------------------------------------------"
+echo "2 - Make Required Files Executable"
+echo ""
+chmod +x *py
+chmod -x config.py
+chmod +x *sh
+echo "Done Permissions"
+echo "-------------------------------------------------------------"
+echo "3 - Performing Raspbian System Update"
+echo "    This Will Take Some Time ...."
+echo ""
+sudo apt-get -y update
+echo "Done Update"
+echo "-------------------------------------------------------------"
+echo "4 - Performing Raspbian System Upgrade"
+echo "    This Will Take Some Time ...."
+echo ""
+sudo apt-get -y upgrade
+echo "Done Upgrade"
+echo "-------------------------------------------------------------"
+echo "5 - Installing pi-timolo Dependencies"
+echo ""
+sudo apt-get install -yq python-picamera python-imaging dos2unix python-pyexiv2 libav-tools
+sudo apt-get install -yq fonts-freefont-ttf # Required for Jessie Lite Only
+if [ -e gdrive ]; then
+  echo "-------------------------------------------------------------"
+  echo "6 - Install Latest gdrive to /usr/local/bin/gdrive"
+  echo ""
+  sudo chmod +x gdrive
+  sudo cp gdrive /usr/local/bin
+  /usr/local/bin/gdrive | grep version
+  echo "Done gdrive Install"
+else
+  echo "Error - Could not find gdrive file in current folder.  Please investigate ..."
+  echo "        pi-timolo syncing feature will not be available."
+fi
+
+cd $DIR
+# Check if pi-timolo-install.sh was launched from pi-timolo folder
+if [ "$DIR" != "$INSTALL_PATH" ]; then
+  if [ -e 'pi-timolo-install.sh' ]; then
+    echo "$STATUS Cleanup pi-timolo-install.sh"
+    rm pi-timolo-install.sh
+  fi
+fi
+echo "Done Dependencies"
+echo "-----------------------------------------------"
+echo "7 - $STATUS Complete"
+echo "-----------------------------------------------"
+echo ""
+echo "See Readme.md for pi-timolo Program"
+echo "Requirements, Configuration and Setting up gdrive security token"
+echo
+echo "Note:"
+echo "1 - Reboot RPI if there are significant Raspbian system updates."
+echo "2 - If config.py already exists then old file named config.py.prev"
+echo "3 - Check pi-timolo variable settings in config.py. See comments for details."
+echo "    cd ~/pi-timolo"
+echo "    nano config.py"
+echo "    ctrl-x y to save and quit nano editor"
+echo "4 - To Run pi-timolo perform the following in SSH or terminal session"
+echo ""
+echo "    cd ~/pi-timolo"
+echo "    ./pi-timolo.py"
+echo ""
+echo "-------------------------------------------------------------"
+echo "See Readme.md or GitHub wiki for Further Details"
+echo ""
+echo $TIMOLO_DIR "Good Luck Claude ..."
+echo "Bye"
+
