099209ac182d016ef52d3ca6bd0a104a41d074d3
Claude Pageau <pageauc@gmail.com>
2016-12-11 21:14:13 -0600

    Updated webserver to add features and run as daemon
    See code comments for details and variables

--- source/webserver.py
+++ source/webserver.py
@@ -3,7 +3,7 @@ import cgi, os, SocketServer, sys, time, urllib
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from StringIO import StringIO
 
-version = "ver 1.1"
+version = "ver 1.3"
 # SimpleHTTPServer python program to allow selection of images from right panel and display in an iframe left panel
 # Use for local network use only since this is not guaranteed to be a secure web server.
 # based on original code by zeekay and modified by Claude Pageau Nov-2015 for use with pi-timolo.py on a Raspberry Pi
@@ -13,17 +13,12 @@ version = "ver 1.1"
 # 2 - On Terminal session execute
 # python ./webserver.py
 # 3 - On a web browser url bar input the server ip address and port number
-# eg 192.168.1.110:8000
-# 4 - If you wish to launch in the background then add /etc/init.d script (copied from skeleton and edit accordingly) then initialize script
-# eg
-# chmod +x webserver.py
-# cd /etc/init.d
-# sudo cp skeleton webserver
-# sudo nano webserver      (change variables in script to point to location of webserver.py) 
-# sudo chmod +x webserver
-# sudo update-rc.d webserver defaults
-# sudo webserver       (test to make sure it works and trouble shoot as necessary)
-# Reboot to test that webserver auto starts on boot.
+# eg 192.168.1.110:8080
+# 4 - To Run this script as a background daemon execute the following command
+#     ./webserver.sh start
+#     or to check status   
+#     ./webserver.sh
+#     You can then close the console session.
 
 # Web Server settings
 web_server_root = "/home/pi/pi-timolo/motion"  # path to webserver image folder
@@ -34,6 +29,7 @@ web_page_refresh_sec ="60"                     # Refresh page time default=60 se
 # Size of Images to display 
 image_width = "1280"
 image_height = "720"
+image_max_listing = 0     # 0=All or Specify Max left side entries
 
 # Left side image iframe settings to same as image dimensions 
 image_frame_width = image_width   
@@ -42,6 +38,20 @@ image_frame_height = image_height
 # Settings for right side files list
 show_by_datetime = True           # True=datetime False=filename
 sort_descending = True            # reverse sort order (filename or datetime per show_by_date setting
+
+if show_by_datetime:
+    dir_sort = 'Date/Time'
+else:
+    dir_sort = 'File Name'
+
+if sort_descending:
+    dir_order = 'Descend'
+else:
+    dir_order = 'Ascend'
+
+list_title = "by %s %s" % ( dir_sort, dir_order )
+img_point = 1          # Item in list to display first Note skip 0 since it might be in progress    
+
 list_height = image_frame_height  # height of directory listing on right side 
 
 class DirectoryHandler(SimpleHTTPRequestHandler):
@@ -49,16 +59,14 @@ class DirectoryHandler(SimpleHTTPRequestHandler):
     def list_directory(self, path):
         try:
             list = os.listdir(path)
+            list_len = len(list)           
         except os.error:
             self.send_error(404, "No permission to list directory")
             return None
         if show_by_datetime:
-            list_title = "by DateTime decending=%s" % sort_descending
             list.sort(key=lambda x: os.stat(os.path.join(path, x)).st_mtime,reverse=sort_descending)   # Sort by most recent modified date/time first
         else:
-            list_title = "by FileName decending=%s" % sort_descending       
             list.sort(key=lambda a: a.lower(),reverse=sort_descending)
-            
         f = StringIO()
         displaypath = cgi.escape(urllib.unquote(self.path))
         # Start HTML formatting code
@@ -68,14 +76,22 @@ class DirectoryHandler(SimpleHTTPRequestHandler):
         f.write("<body>\n")
         # Start Left iframe Image Panel
         f.write('<iframe width="%s" height="%s" align="left"' % (image_frame_width, image_frame_height))
-        f.write('src="%s" name="imgbox" id="imgbox" alt="%s">' % (list[0], web_page_title))  # display first image in list
+        f.write('src="%s" name="imgbox" id="imgbox" alt="%s">' % (list[1], web_page_title))  # display first image in list
         f.write('<p>iframes are not supported by your browser.</p></iframe>')
         # Start Right File selection List Panel
         list_style = '<div style="height: ' + list_height + 'px; overflow: auto; white-space: nowrap; ">'
         f.write(list_style)
-        f.write('<center><b>%s</b></center><hr><ul>' % list_title)
+        f.write('<center><b>%s</b></center><hr>' % list_title)
+        f.write('<ul name="menu" id="menu">')        
         # Create the formatted list of right panel hyperlinks to files in the specified directory
+        
+        list_counter = 0
+        total_entries = 0
         for name in list:
+            if image_max_listing > 1:
+                list_counter += 1
+                if list_counter > image_max_listing:
+                    break
             fullname = os.path.join(path, name)
             displayname = linkname = name
             date_modified = time.ctime(os.path.getmtime(fullname))
@@ -86,9 +102,11 @@ class DirectoryHandler(SimpleHTTPRequestHandler):
             if os.path.islink(fullname):
                 displayname = name + "@"
                 # Note: a link to a directory displays with @ and links with /
+            total_entries += 1                
             f.write('<li><a href="%s" target="imgbox">%s</a> - %s</li>\n'
-                    % (urllib.quote(linkname), cgi.escape(displayname), date_modified))
-        f.write('</ul><hr></div><p><center><b>%s</b></center></p></body>\n</html>\n' % web_server_root)
+                        % ( urllib.quote(linkname), cgi.escape(displayname), date_modified))
+        f.write('</ul><hr></div>')
+        f.write('<p><center><b>%s  ( Total Entries=%i ) </b></center></p>' % (web_server_root, total_entries))
         length = f.tell()
         f.seek(0)
         self.send_response(200)
@@ -100,17 +118,32 @@ class DirectoryHandler(SimpleHTTPRequestHandler):
 
 # Start Web Server Processing        
 os.chdir(web_server_root)
+SocketServer.TCPServer.allow_reuse_address = True
 httpd = SocketServer.TCPServer(("", web_server_port), DirectoryHandler)
-print ("---------------------------- Settings --------------------------")
-print ("Server - web_server_root=%s" % ( web_server_root ))
-print ("         web_server_port=%i  version=%s" % ( web_server_port, version ))
-print ("Images - image_width=%s  image_height=%s" % ( image_width, image_height ))
-print ("Sort   - show_by_datetime=%s  sort_decending=%s" % ( show_by_datetime, sort_descending ))
-print ("----------------------------------------------------------------")
-print ("              Use a Web Browser to access this server at")
-print ("http://IP_Address:%i"  % web_server_port)
-print ("Where IP_Address is IP address of this Raspberry Pi Eg. http://192.168.1.100:%i" % web_server_port)
-print ("----------------------------------------------------------------")
-httpd.serve_forever()
+print("---------------------------- Settings --------------------------")
+print("Server - web_server_root=%s" % ( web_server_root ))
+print("         web_server_port=%i  version=%s" % ( web_server_port, version ))
+print("Images - image_width=%s  image_height=%s" % ( image_width, image_height ))
+print("Sort   - show_by_datetime=%s  sort_decending=%s" % ( show_by_datetime, sort_descending ))
+print("----------------------------------------------------------------")
+print("From a computer on the same LAN. Use a Web Browser to access this server at")
+print("http://IP_Address:%i"  % web_server_port)
+print("Where IP_Address is IP address of this Raspberry Pi Eg. http://192.168.1.100:%i" % web_server_port)
+print("")
+print("IMPORTANT: If You Get - socket.error: [Errno 98] Address already in use")
+print("           Wait a minute or so for webserver to timeout and Retry.")
+print("              ctrl-c to exit this webserver script")
+print("----------------------------------------------------------------")
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("")
+    print("User Stopped webserver.py  Bye.")
+    httpd.shutdown()
+    httpd.socket.close()   
+except IOError as e:
+    print("I/O error({0}): {1}".format(e.errno, e.strerror))
+
+
 
 
---
+++ source/webserver.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script will run the webserver.py as a background task
+# You will then be able close the terminal session.  To auto start
+# Add the following command to the /etc/rc.local
+# /home/pi/pi-timolo/webserver.sh start
+
+progpath="/home/pi/pi-timolo"
+progname="webserver.py"
+
+echo "$0 ver 1.0 written by Claude Pageau"
+echo "-----------------------------------------------"
+cd $progpath
+
+# Check if progname exists
+if [ ! -e $progname ] ; then
+  echo "ERROR - Could Not Find $progname"
+  exit 1
+fi
+
+if [ -z "$( pgrep -f $progname )" ]; then
+  if [ "$1" = "start" ]; then
+     echo "Starting $progname ...."
+     ./$progname >/dev/null 2>&1 &
+  fi
+else
+  if [ "$1" = "stop" ]; then
+    echo "Stopping $progname ...."
+    webserverPID=$( pgrep -f $progname )
+    sudo kill $webserverPID
+  fi
+fi
+
+if [ -z "$( pgrep -f $progname )" ]; then
+    echo "$progname is Not Running ..."
+    echo "To Start $progname execute command below"
+    echo "sudo $0 start"
+  else
+    webserverPID=$(pgrep -f $progname)
+    echo "$progname is Running ..."
+    echo "PID is $webserverPID"
+    echo "To Stop $progname execute command below"
+    echo "sudo $0 stop"
+fi
+echo "Bye"
