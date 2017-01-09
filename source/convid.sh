57781271d5934e48ddbe7d60565427a01daa3cab
Claude Pageau <pageauc@gmail.com>
2017-01-09 07:44:57 -0600

    ver 0.7 Now processes latest h264 video

--- source/convid.sh
+++ source/convid.sh
@@ -7,51 +7,44 @@
 #
 # The converted h264 can be deleted or renamed per del_h264 setting
 #
-# To Automate Add a crontab entry to crontab per the below.  
+# To Automate Add a crontab entry to crontab per the below.
 # make sure convid.sh is executable eg sudo chmod +x convid.sh
 #
 # crontab -e
 #
-# Add similar crontab entry as line below (excluding the #). 
+# Add similar crontab entry as line below (excluding the #).
 # This would execute convid.sh every minute
 #
 # */1 * * * * /home/pi/pi-timolo/convid.sh > /dev/null
-ver="0.7"
+ver="0.8"
 
 echo "$0 version $ver by Claude Pageau"
 echo "Batch Convert h264 to MP4 using MP4Box"
 echo "------------------------------------------"
+
 cd /home/pi/pi-timolo   # change directory
 
-# --------- Variable Settings ---------------
-del_h264=true   # delete=true rename=false
+# Variable Settings
+del_h264=true     # delete=true rename=false
 source_files=/home/pi/pi-timolo/motion/*h264
 
 command_to_run='/usr/bin/MP4Box -add '
+
 # looking for files matching $source_files
 # ls -t sorts files by last modification time, most recent to oldest
 # IMPORTANT  - The latest file will not be processed since it may be being written to
-ls -t $source_files |
-(
 
-  # the first line will be the most recent file
-
-  read the_most_recent_file
-  # do nothing with the most recent file: $the_most_recent_file
-
-  # the rest of the lines will not be the most recent file
-
-  # do something with the rest of the files
-  while read not_the_most_recent_file
-  do
-    if [ -z "$( pgrep -f MP4Box )" ]; then   # Does file exist
-        if [ ! -e $not_the_most_recent_file ]; then
-            echo "ERROR  - Could Not Find File $not_the_most_recent_file"
+function convert ()  #Convert h264 file to MP4
+{
+     file=$1
+     if [ -z "$( pgrep -f MP4Box )" ]; then   # Does file exist
+        if [ ! -e $file ]; then
+            echo "ERROR  - Could Not Find File $file"
             echo "         Please Investigate"
         else
-            echo "START  - Converting $not_the_most_recent_file" 
-            MP4filename=$(echo $not_the_most_recent_file | cut -f 1 -d '.')
-            $command_to_run $not_the_most_recent_file $MP4filename.mp4
+            echo "START  - Converting $file"
+            MP4filename=$(echo $file | cut -f 1 -d '.')
+            $command_to_run $file $MP4filename.mp4
             if [ $? -ne 0 ] ; then
                 echo "------------------------------------------"
                 echo "ERROR   -  Problem running $command_to_run"
@@ -60,22 +53,45 @@ ls -t $source_files |
                 echo "sudo apt-get -y install gpac"
                 exit 1
             else
-                touch -r $not_the_most_recent_file $MP4filename.mp4                
-                if [ "$del_h264" = true ]; then                  
+                touch -r $file $MP4filename.mp4
+                if [ "$del_h264" = true ]; then
                     echo "STATUS - Deleting $not_the_most_recent_file"
-                    rm $not_the_most_recent_file    
+                    rm $file
                 else
-                    echo "STATUS - Rename $not_the_most_recent_file $not_the_most_recent_file.done"
-                    rm -f $not_the_most_recent_file.done
-                    mv $not_the_most_recent_file $not_the_most_recent_file.done
-                fi                    
+                    echo "STATUS - Rename $file $file.done"
+                    rm -f $file.done
+                    mv $file $file.done
+                fi
             fi
-            echo "DONE   - Processing of $not_the_most_recent_file"
+            echo "DONE   - Processing of $file"
             echo "================================================"
         fi
     else
         echo "MP4Box is Already Running ..."
     fi
+}
+
+#Check if there are files to process
+if ls $source_files 1> /dev/null 2>&1; then
+    echo "STATUS- Found $source_files Files"
+else
+    echo "WARN  - No $source_files Files Found to Process."
+    exit
+fi
+
+ls -t $source_files |
+(
+  # the first line will be the most recent file
+  read the_most_recent_file
+  echo $the_most_recent_file "May Still be Recording"
+  echo "Wait 15 seconds Before Processing."
+  sleep 15   # Wait in case video is in progress
+  convert $the_most_recent_file
+  # Process the rest of the files
+  while read not_the_most_recent_file
+  do
+     convert $not_the_most_recent_file
   done
 )
+
 echo "$0 Done"
