21408f315e13511893fc62c3c090c3dc8b1732be
Colby Adams <colbysadams480@gmail.com>
2017-03-21 02:03:51 -0500

    Gave motion detection a complete overhaul. Added a dependency to scipy.stats.  Required to calculate some of the probabilities.

--- source/config.py
+++ source/config.py
@@ -26,18 +26,18 @@ imageVFlip = False         # Flip image Vertically    default=False
 imageHFlip = False         # Flip image Horizontally  default=False
 imageRotation = 0          # Rotate image. Valid values: 0, 90, 180 & 270
 noNightShots = False       # Don't Take images at Night default=False
-noDayShots = False         # Don't Take images during day time default=False  
+noDayShots = False         # Don't Take images during day time default=False
 imagePreview = False       # Preview image on connected RPI Monitor default=False
 
 # Low Light Night Settings
-twilightThreshold = 40     # default=40 Light level to trigger day/night transition at twilight 
-nightSleepSec = 20         # default=20 Sec - Time period to allow camera to calculate low light AWB 
-nightMaxShut = 5.5         # default=5.5 sec Highest cam shut exposure time. 
+twilightThreshold = 40     # default=40 Light level to trigger day/night transition at twilight
+nightSleepSec = 20         # default=20 Sec - Time period to allow camera to calculate low light AWB
+nightMaxShut = 5.5         # default=5.5 sec Highest cam shut exposure time.
                            # IMPORTANT 6 sec works sometimes but occasionally locks RPI and HARD reboot required to clear
 nightMinShut = .002        # default=.002 sec Lowest camera shut exposure time for transition from day to night (or visa versa)
 nightMaxISO = 800          # default=800  Max cam ISO night setting
-nightMinISO = 100          # lowest ISO camera setting for transition from day to night (or visa versa)  
- 
+nightMinISO = 100          # lowest ISO camera setting for transition from day to night (or visa versa)
+
 # Date/Time Settings for Displaying info Directly on Images
 showTextFontSize = 18      # Size of image Font in pixel height
 showDateOnImage = True     # Set to False for No display of date/time on image default= True
@@ -51,13 +51,13 @@ motionPrefix = "mo-"       # Prefix Motion Detection images
 motionDir = "media/motion"       # Storage Folder for Motion Detect Images
 threshold = 35             # How much a pixel has to change to be counted default=35 (1-200)
 sensitivity = 100          # Number of changed pixels to trigger motion default=100
-motionAverage = 2          # Number of images to average for motion verification: 1=last image only or 100=Med 300=High Average Etc.
-useVideoPort = False       # Use the video port to capture motion images - faster than the image port. Default=False
+motionAverage = 20         # Number of images to average for motion verification: 1=last image only or 100=Med 300=High Average Etc.
+useVideoPort = True        # Use the video port to capture motion images - faster than the image port. Default=False
 motionVideoOn = False      # If set to True then video clip is taken rather than image
 motionVideoTimer = 10      # Number of seconds of video clip to take if Motion Detected default=10
 motionQuickTLOn = False    # if set to True then take a quick time lapse sequence rather than a single image (overrides motionVideoOn)
 motionQuickTLTimer = 10    # Duration in seconds of quick time lapse sequence after initial motion detected default=10
-motionQuickTLInterval = 0  # Time between each Quick time lapse image 0 is fast as possible 
+motionQuickTLInterval = 0  # Time between each Quick time lapse image 0 is fast as possible
 motionForce = 3600         # Force single motion image if no Motion Detected in specified seconds.  default=3600 1 hr
 motionNumOn = True         # True=On (filenames by sequenced Number) otherwise date/time used for filenames
 motionNumStart = 1000      # Start motion number sequence
@@ -65,7 +65,7 @@ motionNumMax  = 500        # Max number of motion images desired. 0=Continuous  
 motionNumRecycle = True    # After numberMax reached restart at numberStart instead of exiting default=True
 motionMaxDots = 100        # Number of motion dots before starting new line
 createLockFile = False     # default=False if True then sync.sh will call gdrive to sync files to your web google drive if .sync file exists
-                           # Lock File is used to indicate motion images are added so sync.sh can sync in background via sudo crontab -e 
+                           # Lock File is used to indicate motion images are added so sync.sh can sync in background via sudo crontab -e
 
 # Time Lapse Settings
 timelapseOn = False        # Turns timelapse True=On  False=Off
@@ -73,9 +73,9 @@ timelapsePrefix = "tl-"    # Prefix timelapse images with this prefix
 timelapseDir = "media/timelapse" # Storage Folder for Time Lapse Images
 timelapseTimer = 300       # Seconds between timelapse images  default=300 5 min
 timelapseNumOn = True      # True=On (filenames Sequenced by Number) otherwise date/time used for filename
-timelapseNumStart = 1000   # Start of timelapse number sequence 
+timelapseNumStart = 1000   # Start of timelapse number sequence
 timelapseNumMax = 2000     # Max number of timelapse images desired. 0=Continuous  default=2000
-timelapseNumRecycle = True # After numberMax reached restart at numberStart instead of exiting default=True   
+timelapseNumRecycle = True # After numberMax reached restart at numberStart instead of exiting default=True
 timelapseExit = 0          # Will Quit program after specified seconds 0=Continuous  default=0
 
 #======================================
@@ -86,7 +86,7 @@ timelapseExit = 0          # Will Quit program after specified seconds 0=Continu
 web_image_height = "768"       # px height of images to display in iframe default 768
 web_iframe_width_usage = "75%" # Left Pane - Sets % of total screen width allowed for iframe. Rest for right list
 web_iframe_width = "100%"      # Desired frame width to display images. can be eg percent "80%" or px "1280"
-web_iframe_height = "100%"     # Desired frame height to display images. Scroll bars if image larger (percent or px) 
+web_iframe_height = "100%"     # Desired frame height to display images. Scroll bars if image larger (percent or px)
 
 # Right Side Files List
 web_max_list_entries = 0         # 0 = All or Specify Max right side file entries to show (must be > 1)
@@ -98,14 +98,9 @@ web_list_sort_descending = True  # reverse sort order (filename or datetime per 
 web_server_root = "media"     # webserver root path to webserver image folder
 web_server_port = 8080        # Web server access port eg http://192.168.1.100:8090
 web_page_title = "Pi-Timolo Media"     # web page title that browser show (not displayed on web page)
-web_page_refresh_on = True    # False=Off (never)  Refresh True=On (per seconds below)       
+web_page_refresh_on = True    # False=Off (never)  Refresh True=On (per seconds below)
 web_page_refresh_sec = "180"  # seconds to wait for web page refresh default=180 seconds (three minutes)
 web_page_blank = False        # Start left image with a blank page until a right menu item is selected
                               # Otherwise False displays second list[1] item since first may be in progress
 
 # ---------------------------------------------- End of User Variables -----------------------------------------------------
-
-
-
-
-
