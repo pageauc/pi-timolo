9fbccebbb01695f7712a3f68a76229a8ce6d6db3
Claude Pageau <pageauc@gmail.com>
2017-12-25 10:49:57 -0600

    remove test for existing install install

--- source/rclone-install.sh
+++ source/rclone-install.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
-ver="9.6"
+ver="9.7"
 echo "$0 ver $ver written by Claude Pageau"
-
-if [ ! -f /usr/bin/rclone -o ! -z "$1" ]; then
-    echo "-------------------------------------------------------------------------------"
-    echo "Download https://downloads.rclone.org/rclone-v1.38-linux-arm.zip"
-    wget wget -O rclone.zip -q --show-progress https://downloads.rclone.org/rclone-current-linux-arm.zip
-    echo "unzip rclone.zip to folder rclone-tmp"
-    unzip -o -j -d rclone-tmp rclone.zip
-    echo "Install files and man pages"
-    cd rclone-tmp
-    sudo cp rclone /usr/bin/
-    sudo chown root:root /usr/bin/rclone
-    sudo chmod 755 /usr/bin/rclone
-    sudo mkdir -p /usr/local/share/man/man1
-    sudo cp rclone.1 /usr/local/share/man/man1/
-    sudo mandb
-    cd ..
-    echo "Deleting rclone.zip and Folder rclone-tmp"
-    rm rclone.zip
-    rm -r rclone-tmp
-    wget -O rclone-sync.sh https://raw.github.com/pageauc/pi-timolo/master/source/rclone-sync.sh
-    chmod +x rclone-sync.sh
-fi
+echo "-------------------------------------------------------------------------------"
+echo "Download https://downloads.rclone.org/rclone-v1.38-linux-arm.zip"
+wget wget -O rclone.zip -q --show-progress https://downloads.rclone.org/rclone-current-linux-arm.zip
+echo "unzip rclone.zip to folder rclone-tmp"
+unzip -o -j -d rclone-tmp rclone.zip
+echo "Install files and man pages"
+cd rclone-tmp
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp rclone.1 /usr/local/share/man/man1/
+sudo mandb
+cd ..
+echo "Deleting rclone.zip and Folder rclone-tmp"
+rm rclone.zip
+rm -r rclone-tmp
+wget -O rclone-sync.sh https://raw.github.com/pageauc/pi-timolo/master/source/rclone-sync.sh
+chmod +x rclone-sync.sh
 
 if [ -f /usr/bin/rclone ]; then
-  echo "$STATUS rclone is installed at /usr/bin/rclone"
+  echo "rclone is installed at /usr/bin/rclone"
   rclone -V
   echo "                       INSTRUCTIONS
 1 You will be required to have a login account on the remote service
@@ -46,19 +43,17 @@ if [ -f /usr/bin/rclone ]; then
 8 To test remote service access. Execute the following where
   gdmedia is the name you gave your remote service
 
+  rclone listremotes
   rclone ls gdmedia:/
 
-Example sync command make source identical to destination
+Example sync command will make source identical to destination.
 
 rclone sync -v /home/pi/pi-timolo/media/motion gdmedia:media/motion
 
-To upgrade
-
-  ./rclone-install.sh upgrade
-
-Note: Instructions were tested with google drive."
+Note: Instructions were tested with google drive.
+"
 else
-  echo "$STATUS Problem Installing rclone.  Please Investigate"
+  echo "Problem Installing rclone.  Please Investigate"
 fi
 echo "Bye..."
 
