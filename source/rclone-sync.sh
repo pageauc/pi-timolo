745174c2a632d0ffd8c7793bac477c271bb5f6e2
Claude Pageau <pageauc@gmail.com>
2017-12-31 06:40:01 -0600

    Updated error checking

--- source/rclone-sync.sh
+++ source/rclone-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 progName=$(basename -- "$0")
-echo "$progName ver 9.6  written by Claude Pageau"
+echo "$progName ver 9.71  written by Claude Pageau"
 
 # Customize rclone sync variables Below
 # ---------------------------------------
@@ -11,8 +11,7 @@ remoteSyncDir="mycam/recent"
 # ---------------------------------------
 
 # Display Users Settings
-echo "
----------- SETTINGS -------------
+echo "----------- SETTINGS -------------
 rcloneName   : $rcloneName
 syncRoot     : $syncRoot
 localSyncDir : $localSyncDir
@@ -22,15 +21,42 @@ if pidof -o %PPID -x "$progName"; then
     echo "WARN  - $progName Already Running. Only One Allowed."
 else
     if [ -f /usr/bin/rclone ]; then
-        echo "rclone is installed at /usr/bin/rclone"
-        rclone -V
-        echo "cd $syncRoot"
-        cd "$syncRoot"
-        echo "/usr/bin/rclone sync -v $localSyncDir $rcloneName:$remoteSyncDir"
-        echo "One Moment Please ..."
-        /usr/bin/rclone sync -v $localSyncDir $rcloneName:$remoteSyncDir
-    else 
-        echo "WARN  - /usr/bin/rclone Not Installed."
-    fi    
+        rclone -V   # Display rclone version
+        if [ ! -d "$localSyncDir" ] ; then
+           echo "---------------------------------------------------"
+           echo "ERROR - localSyncDir=$localSyncDir Does Not Exist."
+           echo "        Please Investigate Bye ..."
+           exit 1
+        fi
+
+        /usr/bin/rclone listremotes | grep "$rcloneName"
+        if [ $? == 0 ]; then
+           echo "/usr/bin/rclone sync -v $localSyncDir $rcloneName:$remoteSyncDir"
+           echo "One Moment Please ..."
+           /usr/bin/rclone sync -v $localSyncDir $rcloneName:$remoteSyncDir
+           if [ ! $? -eq 0 ]; then
+               echo "---------------------------------------------------"
+               echo "ERROR - rclone sync failed. Review output for Possible Cause"
+           else
+               echo "INFO  - Sync Successful ..."
+           fi
+        else
+           echo "---------------------------------------------------"
+           echo "ERROR - remoteName $rcloneName Does not Exist"
+           echo "List of Remote Names"
+           echo "-------------------"
+           rclone listremotes
+           echo "--------------------"
+           echo "If List is Empty"
+           echo "Configure a New Remote Storage Name per Command below"
+           echo "    rclone config"
+           echo "or Check Spelling of Variable rcloneName=$rcloneName"
+           echo "For more Details See Readme.txt"
+        fi
+    else
+        echo "ERROR  - /usr/bin/rclone Not Installed."
+        echo "         You Must Install and Configure rclone"
+    fi
 fi
+echo "---------------------------------------------------"
 echo "$progName Bye ..."
