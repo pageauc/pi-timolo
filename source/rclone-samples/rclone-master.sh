7c2b52d906ec43819bba25f103db41a8428c55ab
Claude Pageau <pageauc@gmail.com>
2018-01-16 06:01:08 -0600

    minor fix

--- source/rclone-samples/rclone-master.sh
+++ source/rclone-samples/rclone-master.sh
@@ -16,7 +16,7 @@ echo "$progName $ver  written by Claude Pageau"
 lockFileCheck=false       # true= Checks for pi-timolo.sync file. false = No Check (case sensitive)
 rcloneName="gdmedia"     # Name of Remote Storage Service
 syncRoot="/home/pi/pi-timolo"       # Root Folder to Start
-localDir="media/rclone-samples"     # Source Folder on Local
+localDir="rclone-samples"           # Source Folder on Local
 remoteDir="mycam/rclone-samples"    # Destination Folder on Remote
 rcloneParam="sync"       # rclone option to perform  Eg  sync, copy, move
                          # IMPORTANT: sync will make remoteDir identical to localDir
